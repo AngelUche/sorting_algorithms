401912b5d37b58be82bce2f93dc9ade4fb6a37f6
AngelUche <106807725+AngelUche@users.noreply.github.com>
2022-11-09 08:53:08 +0000

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,4 +7,30 @@ include "sort.h"
  *
  * Return: void
  */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *swap_node, *next_swap;
 
+	if (list == NULL || *list == NULL)
+		return;
+	swap_node = (*list)->next;
+	while (swap_node != NULL)
+	{
+		next_swap = swap_node->next;
+		while (swap_node->prev != NULL && swap_node->prev->n > swap_node->n)
+		{
+			swap_node->prev->next = swap_node->next;
+			if (swap_node->next != NULL)
+				swap_node->next->prev = swap_node->prev;
+			swap_node->next = swap_node->prev;
+			swap_node->prev = swap_node->next->prev;
+			swap_node->next->prev = swap_node;
+			if (swap_node->prev == NULL)
+				*list = swap_node;
+			else
+				swap_node->prev->next = swap_node;
+			print_list(*list);
+		}
+		swap_node = next_swap;
+	}
+}
